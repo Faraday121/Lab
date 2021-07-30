3931b5c86fe63d22a29f0fb854d15c14bb95962d
feng1 <feng1040929519@gmail.com>
2021-07-30 19:30:23 +0800

    1

---
+++ Lab/src/main/java/Main/Lab1.java
@@ -0,0 +1,11 @@
+package Main;
+
+import util.MyUtils;
+
+public class Lab1 {
+
+    public static void main(String[] args) {
+        int a = MyUtils.relativeError(1,2);
+        System.out.println(a);
+    }
+}
