1714d28bf0ad51cea27a15b159dbba3f2646d0fc
feng1 <feng1040929519@gmail.com>
2021-07-31 10:12:41 +0800

    1

--- Lab/src/main/java/Main/Lab1.java
+++ Lab/src/main/java/Main/Lab1.java
@@ -1,12 +1,64 @@
 package Main;
 
-
-import util.ErrorUtils;
+import static java.lang.Math.*;
 
 public class Lab1 {
 
-    public static void main(String[] args) {
-        int a = ErrorUtils.error(1,2);
-        System.out.println(a);
+    final static double pai = 3.14;
+    final static double g = 9.8;
+
+    public static double Q1A(double x ){
+        return 4.3+3.2*x+8*(pow(x,2));
     }
+
+    public static double Q1B(double x ){
+        return 6*pai+1/(sqrt(4+3*x));
+    }
+
+    public static double Q1C(double v, double g ,double o){
+        //o = 15,30,45,60,75...
+        return (2*pow(v,2)*sin(o)*cos(o))/g;
+    }
+
+    public static double Q2(double x ){
+        //initial velocity(m/s)
+        double v = 80;
+        //initial height(m)
+        double y0 = 50;
+        //angle(degree)
+        double o =40;
+        return y0+ tan(o)*x -(g)/(2*pow(v,2)*pow(cos(o),2))*(pow(x,2));
+    }
+
+    public static double Q3A(double t){
+        //velocity by time
+        return (2*t)/(sqrt(1+pow(t,2)));
+    }
+
+    public static double Q3B(double t){
+        //acceleration(dv/dt)
+        //t = 5.0,v=
+        return (2)/((1+pow(t,2))*(sqrt(1+pow(t,2))));
+    }
+
+    public static double Q3C(double t, double h){
+        //central difference formula
+        //difference in the y-values / difference in the x-values
+
+        //assume h = 0.2
+        return (Q3A(t+h)-Q3A(t))/h;
+    }
+
+    public static double[] Q4A(){
+        double[] f = {1.49,1.82,2.23,2.72,3.32,4.06,5.95};
+        //double[] x = {0.2,0.3,0.4,0.5,0.6,0.7,0.8};
+
+        double[] diff = new double[7];
+        for (int i = 0; i <diff.length-1 ; i++) {
+            diff[i]=f[i+1]-f[i];
+        }
+        return diff;
+    }
+
+
 }
---
+++ Lab/src/test/java/Main/Lab1Test.java
@@ -0,0 +1,56 @@
+package Main;
+
+public class Lab1Test {
+
+    @org.junit.Test
+    public void q1A() {
+        System.out.println(Lab1.Q1A(3.6));
+    }
+
+    @org.junit.Test
+    public void q1B() {
+        System.out.println(Lab1.Q1B(19.4));
+    }
+
+    @org.junit.Test
+    public void q1C() {
+        double v = 10;
+        double g =9.81;
+        double[] o = {15,30,45,60,75};
+        for (double value : o) {
+            double result = Lab1.Q1C(v, g, value);
+            System.out.println("o is " + value + ", result is" + result);
+        }
+    }
+
+    @org.junit.Test
+    public void q2() {
+        //take x from 1 to 10;
+        double[] x = {1,2,3,4,5,6,7,8,9,10};
+        for (double v : x) {
+            double result = Lab1.Q2(v);
+            System.out.println(result);
+        }
+    }
+
+    @org.junit.Test
+    public void q3() {
+
+        double result1 = Lab1.Q3B(5.0);
+        System.out.println("using the t = 5.0s , the acceleration is :"+result1);
+
+        double result2 = Lab1.Q3C(5.0,0.2);
+        System.out.println("using the h = 0.2 , the acceleration is :"+result2);
+
+    }
+
+    @org.junit.Test
+    public void Q4(){
+        double[] diff = Lab1.Q4A();
+        for (double v : diff) {
+            System.out.println(v);
+        }
+    }
+
+
+}
