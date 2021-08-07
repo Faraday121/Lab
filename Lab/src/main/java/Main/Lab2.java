e4701cde45bb994e174670a38234663c1099accb
feng1 <feng1040929519@gmail.com>
2021-08-07 11:48:54 +0800

    1

--- Lab/src/main/java/Main/Lab2.java
+++ Lab/src/main/java/Main/Lab2.java
@@ -1,10 +1,8 @@
 package Main;
 
 
-import static java.lang.Math.*;
-import org.apache.commons.math3.analysis.polynomials.PolynomialFunctionLagrangeForm;
 import util.ErrorUtils;
-
+import static java.lang.Math.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -26,7 +24,6 @@ public class Lab2 {
     }
 
     public static double Q2FunctionDiff(double x){
-        //FIRST DERIVATIVE:
         double power = (double) 3/2;
         return (x*(4*sin(x)-x*cos(x)+4))/(2*pow(sin(x)+1,power));
     }
@@ -57,9 +54,67 @@ public class Lab2 {
         return h2;
     }
 
-    public static double Q3(double actualValue,double expectedValue){
-        return ErrorUtils.percentageError(actualValue,expectedValue);
+    //a = 0, b = 4 , n = 4 exact value =  23.9187
+    public static double Q2Trapezoidal(double a ,double b, double n){
+        return ((b-a)/(2*n))*((2*(Q2TrapezoidalTerms(b,n)))+Q2Function(b));
     }
+    public static double Q2TrapezoidalTerms(double b, double n){
+        double a = 2;
+        double X1 = Q2Function(b-a/n-1);//2/3
+        double X2 = Q2Function(b-a*2/n-1);//4/3
+        double X3 = Q2Function(b-a*3/n-1);//6/3
+        double X4 = Q2Function(b-a*4/n-1);//8/3
+        double X5 = Q2Function(b-a*5/n-1);//10/3
+        return X1+X2+X3+X4+X5;
+    }
+
+    public static double Q2Simpson(double a ,double b){
+        double h = (b-a)/2;
+        return (h/3)*(Q2Function(a)+(4*Q2Function((a+b)/2))+Q2Function(b));
+    }
+
+    final private static double L = 5;
+    final private static double Radius = 1;
+    final private static double height = 0.3;
+    final private static double Length = Radius-height;
+
+    public static double Q3Function(double x){
+        return sqrt(1-x*x);
+    }
+
+    public static double Q3Trapezoidal(){
+        double b = sqrt(pow(Radius,2)-pow(Length,2));
+        double a = 0-b;
+        double n = 6;
+        double h = (b-a)/n;
+        double X1 = Q3Function(a+h);
+        double X2 = Q3Function(a+2*h);
+        double X3 = Q3Function(a+3*h);
+        double X4 = Q3Function(a+4*h);
+        double X5 = Q3Function(a+5*h);
+        double terms = X1+X2+X3+X4+X5;
+        double trapezoidalResult = (h/2)*(Q3Function(abs(a))+2*(terms)+Q3Function(b));
+        return (trapezoidalResult - Length*2*(b))*L;
+    }
+
+    public static double Q3Simpson(){
+        double b = sqrt(pow(Radius,2)-pow(Length,2));
+        double a = 0-b;
+        double n = 2;
+        double h = (b-a)/n;
+        double simpsonResult = (h/3)*(Q3Function(a)+(4*Q3Function((a+b)/2))+Q3Function(b));
+        return (simpsonResult - Length*2*(b))*L;
+    }
+
+//    public static double Q3(){
+//        double horizontalLength = Radius-height;//0.7
+//        double x = sqrt(pow(Radius,2)-pow(horizontalLength,2));
+//        System.out.println(x);
+//        double triangleArea =0.7*0.5*2*x;
+//        double semicircleArea = ((double)152/360)*PI*pow(Radius,2);
+//        double waterArea = semicircleArea - triangleArea;
+//        return waterArea;
+//    }
 
 
 }
--- Lab/src/test/java/Main/Lab2Test.java
+++ Lab/src/test/java/Main/Lab2Test.java
@@ -3,21 +3,24 @@ package Main;
 import org.junit.Test;
 
 import static Main.Lab2.*;
+import util.ErrorUtils;
+
+import static java.lang.Math.*;
 import static org.junit.Assert.*;
 
 public class Lab2Test {
 
     @Test
-    //1day =86400s,90s,45s
     public void q1() {
-        double[] test = {86400,90,45};
+        double[] test = {86400,5400,2700};
         for (int i = 0; i <test.length ; i++) {
             System.out.println(Q1(test[i]));
         }
+        System.out.println("last case is impossible");
     }
 
     @Test
-    public void q2() {
+    public void q2A() {
         double x =3.5;
         double h =0.2;
         double actualValue = Q2A(x,h);
@@ -26,6 +29,25 @@ public class Lab2Test {
         System.out.println("The actual value is:"+actualValue);
         System.out.println("The expected value is:"+ expectedValue);
         System.out.println("The percentage error is:"+ Q2Error(actualValue,expectedValue));
-        System.out.println("To keep the error less than one percent, h is:" + Q2SmallestH(x));
+        System.out.println("To keep the error less than one percent, h is:" + lessError);
+    }
+
+    @Test
+    //a = 0, b = 4 , n = 4 exact value =  23.9187
+    public void Q2B(){
+        double a = 0;
+        double b = 4;
+        double n = 4;
+        double expectedValue = 23.9187;
+        System.out.println("Trapezoidal result is:"+Q2Trapezoidal(a,b,n));
+        System.out.println("percentageError is:" + ErrorUtils.percentageError(Q2Trapezoidal(a,b,n),expectedValue));
+        System.out.println("Simpson result is:"+Q2Simpson(a,b));
+        System.out.println("percentageError is:" + ErrorUtils.percentageError(Q2Simpson(a,b),expectedValue));
+    }
+
+    @Test
+    public void q3(){
+        System.out.println("Trapezoidal result is:"+Q3Trapezoidal());
+        System.out.println("Simpson result is:"+ Q3Simpson());
     }
 }
